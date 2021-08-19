db1eb805a9ae4ee3e7c7de551a64b2037b2d2c6b
m3xpl4y <m3xpl4y@gmail.com>
2021-08-19 11:21:03 +0200

    asp.net musicstore

---
+++ MvcMusicStore/Global.asax.cs
@@ -0,0 +1,27 @@
+using MvcMusicStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace MvcMusicStore
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            //Uncomment to reset database
+            //Database.SetInitializer(new DropCreateDatabaseAlways<MusicStoreDB>());
+            Database.SetInitializer(new MusicStoreDbInitializer());
+
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
---
+++ MvcMusicStore/Models/MusicStoreDbInitializer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using MvcMusicStore.Models;
+
+namespace MvcMusicStore.Models
+{
+    public class MusicStoreDbInitializer : DropCreateDatabaseAlways<MusicStoreDB>
+    {
+        protected override void Seed(MusicStoreDB context)
+        {
+
+            context.Artists.Add(new Artist { Name = "m3xpl4y" });
+            context.Genres.Add(new Genre 
+            { 
+                Name = "Twitch", 
+                Description = "Streaming ASP.NET on Twitch" 
+            });
+            context.Albums.Add(new Album
+            { 
+                Artist = new Artist { Name = "Max Play"},
+                Genre = new Genre { Name = "Youtube", Description = "Videos on Youtube"},
+                Price = 9.99m,
+                Title = "Learning by Doing"
+            });
+
+
+            base.Seed(context);
+        }
+    }
+}
